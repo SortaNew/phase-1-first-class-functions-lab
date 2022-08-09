559fa2022a50e6f04d50b1faf7f53a258d3c03aa
Parry Huang <parryhuang94@gmail.com>
2022-08-09 16:32:21 -0400

    commit tree 4e2ef2146c406e0de931da9d36f1dfd4b55d7443

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (firstTwo) => {
+    const newFirstTwo = firstTwo.slice(0, 2);
+    return newFirstTwo;
+}
+
+const returnLastTwoDrivers = (lastTwo) => {
+    const newLastTwo = lastTwo.slice(lastTwo.length-2);
+    return newLastTwo;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return fare => fare*num; 
+}
+
+function fareDoubler(doubleThis){
+    const doubled = createFareMultiplier(2)(doubleThis);
+    return doubled;
+}
+
+function fareTripler(tripleThis){
+    const tripled = createFareMultiplier(3)(tripleThis);
+    return tripled;
+}
+
+function selectDifferentDrivers(drivers, firstOrLast){
+    if (firstOrLast === returnFirstTwoDrivers)
+        return returnFirstTwoDrivers(drivers);
+    else if (firstOrLast === returnLastTwoDrivers)
+        return returnLastTwoDrivers(drivers);
+}
